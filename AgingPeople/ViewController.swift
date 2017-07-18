c4100e52627c51126fae04d6f72a70f96de83cb5
John Colley <webjocky@gmail.com>
2017-07-18 11:06:47 -0700

    initial commit

--- AgingPeople/ViewController.swift
+++ AgingPeople/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate {
+    var people = ["George", "Betty", "Fran", "Joe", "Helda", "Winifred", "Zed", "Sara", "Jeffy", "Abraham", "Anna", "Melinda"]
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self as UITableViewDataSource
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func getRand() -> Int {
+        return Int(arc4random_uniform(120)+1)
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell:UITableViewCell? =
+            tableView.dequeueReusableCell(withIdentifier: "MyCell")
+        if (cell != nil)
+        {
+            cell = UITableViewCell(style: UITableViewCellStyle.subtitle,
+                                   reuseIdentifier: "MyCell")
+        }
+        cell?.textLabel?.text = people[indexPath.row]
+        cell?.detailTextLabel?.text = "\(getRand()) years old"
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
